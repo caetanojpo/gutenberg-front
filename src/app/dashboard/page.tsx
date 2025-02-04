0db79ebea0213b08e3314a7f0ecf876aabf1ad37
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-04 17:12:55 -0300

    feat: dashboard page initial structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,40 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <main className="h-screen w-screen flex">
+      {/* MENU LATERAL */}
+      <div>
+        <div>LOGO</div>
+        <div>NAVIGATION</div>
+      </div>
+      <div>
+        {/* HEADER */}
+        <div>
+          <div>GREETINGS</div>
+          <div>PROFILE ICON</div>
+        </div>
+        {/* SEARCH BAR */}
+        <div>
+          <div>INPUT</div>
+          <div>BUTTON</div>
+        </div>
+        {/* BOOK LOAD */}
+        <div>
+          <div>
+            <div>BOOK IMAGE</div>
+            <div>
+              <ul>BOOK INFOS</ul>
+            </div>
+          </div>
+          <div>ACTIONS</div>
+        </div>
+        {/* LAST SEEN */}
+        <div>
+          <div>DIVIDER</div>
+          <div>BOOKS GRID</div>
+        </div>
+      </div>
+    </main>
+  );
 }
