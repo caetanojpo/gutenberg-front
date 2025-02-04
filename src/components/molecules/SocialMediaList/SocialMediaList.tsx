5888a09dff5261b9ad8dd1cca0fcff887429daf8
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-04 17:11:58 -0300

    feat: socialMediaList new type

--- src/components/molecules/SocialMediaList/SocialMediaList.tsx
+++ src/components/molecules/SocialMediaList/SocialMediaList.tsx
@@ -1,23 +1,28 @@
-import { socialMedia } from "@/constants/socialMedia";
 import Link from "next/link";
-import React from "react";
 import { SocialMediaItem } from "@/types/socialMedia";
-import Image from "next/image";
+import InstagramIcon from "@/assets/icons/instagram.svg";
+import LinkedinIcon from "@/assets/icons/linkedin.svg";
 
 export default function SocialMediaList() {
+  const socialMedia: SocialMediaItem[] = [
+    {
+      icon: <InstagramIcon className="w-6 h-6" />,
+      text: "Instagram",
+      link: "",
+    },
+    {
+      icon: <LinkedinIcon className="w-6 h-6" />,
+      text: "LinkedIn",
+      link: "",
+    },
+  ];
   return (
     <div className="flex flex-col gap-8 text-[1.2rem]">
       {socialMedia.map(
         ({ icon, text, link }: SocialMediaItem, index: number) => (
           <Link href={link} key={index} className="hover:">
             <div className="flex h-full w-full gap-6 items-center hover:text-highlight">
-              <Image
-                src={icon}
-                alt={text}
-                width={20}
-                height={20}
-                className=""
-              />
+              {icon}
               <p>{text}</p>
             </div>
           </Link>
