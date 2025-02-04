3449b9222082984d0972be6a9bfd59f688eae719
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-04 17:12:16 -0300

    feat: update login component

--- src/components/organisms/Login/Login.tsx
+++ src/components/organisms/Login/Login.tsx
@@ -1,39 +1,15 @@
-import { LoginInput } from "@/components/atoms/Inputs/LoginInput";
 import Image from "next/image";
 import React from "react";
 
+import LoginForms from "@/components/organisms/LoginForms/LoginForms";
+
 export default function Login() {
   return (
-    <div className="flex flex-col w-[60%] h-screen lg:w-screen justify-center items-center text-[1.8rem]">
-      <div className="flex h-fit">
-        <Image src="/full-logo.png" alt="" width={300} height={300} />
+    <div className="flex flex-col w-screen h-screen  justify-center items-center text-[1.8rem] ">
+      <div className="flex h-fit min-w-[280px] min-h-[280px] max-w-[300px] max-h-[300px] relative">
+        <Image src="/full-logo.png" alt="" fill />
       </div>
-      <form method="submit" className="flex flex-col justify-around gap-10">
-        <div className="h-full w-full text-primary text-[2.4rem] font-light">
-          Enter your credentials
-        </div>
-        <div className="flex flex-col gap-8">
-          <div>
-            <LoginInput
-              name="email"
-              label="Email"
-              isRequired
-              textColor="primary"
-            />
-          </div>
-          <div>
-            <LoginInput
-              name="password"
-              label="Password"
-              isRequired
-              textColor="primary"
-            />
-          </div>
-        </div>
-        <div>
-          <button>BUTTON</button>
-        </div>
-      </form>
+      <LoginForms />
     </div>
   );
 }
